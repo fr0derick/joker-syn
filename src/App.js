f857231d08a2604e256f38cf7dacf66b3eb938ec
fr0derick <32341462+fr0derick@users.noreply.github.com>
2024-09-28 02:26:31 +0100

    New logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
-import JokerFinder from './components/JokerFinder';  // Import your JokerFinder component
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import JokerFinder from './components/JokerFinder';  // joker synergies finder
+import DevTools from './components/DevTools';  // dev tools
 
 function App() {
   return (
-    <div className="App">
-      <JokerFinder />  {/* This will render your Joker Synergies Finder */}
-    </div>
+    <Router>
+      <div className="App">
+        {/* Router will handle the navigation between pages */}
+        <Routes>
+          {/* JokerFinder page */}
+          <Route exact path="/" element={<JokerFinder />} />
+          
+          {/* DevTools page */}
+          <Route path="/dev-tools" element={<DevTools />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
